0f7972a069431cfe00693df33ec3784dde36d2b7
oyamanosu <oyamanosu@docomo.ne.jp>
2022-05-25 17:49:32 +0900

    slider_faceexpression add9

--- js/Slider_FaceExpression.js
+++ js/Slider_FaceExpression.js
@@ -1,8 +1,8 @@
 class Slider_FaceExpression{
 
-    constructor(type) {
-        this.type = type;
-      }
+    // constructor(type) {
+    //     this.type = type;
+    //   }
 
     Point_FaceChange(agent_point,limit){
         console.log("Point_FaceChange");
