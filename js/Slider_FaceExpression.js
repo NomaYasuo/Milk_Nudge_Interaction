b500a39a17cbae940172da0ab952a7f8ed36c943
oyamanosu <oyamanosu@docomo.ne.jp>
2022-05-25 16:36:09 +0900

    slider_faceexpression add3

--- js/Slider_FaceExpression.js
+++ js/Slider_FaceExpression.js
@@ -1,5 +1,9 @@
 class Slider_FaceExpresion{
-    
+    constructor(name, number) {
+        this.name = name;
+        this.number = number;
+    }
+
     Point_ChangeFace = (point) => {
         console.log("point",point);
     }
